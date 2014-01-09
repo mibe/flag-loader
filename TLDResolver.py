4aeaeeecd1214b43736ec1db825d981060c6612e
Michael Bemmerl <mail@mx-server.de>
2014-01-09 04:06:09 +0100

    Add a dot to the ccTLD if it is missing.

--- TLDResolver.py
+++ TLDResolver.py
@@ -4,5 +4,9 @@ from WikidataTitleResolver import WikidataTitleResolver
 
 class TLDResolver(WikidataTitleResolver):
     def get_flag(self, tld):
+        # add dot if missing
+        if tld[0] != '.':
+            tld = '.{0}'.format(tld)
+
         # Call base class get_flag method
         return super(TLDResolver, self).get_flag(tld)
