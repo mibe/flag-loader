b10cb2873e229c9af27f266f609bc9adf630fbb1
Michael Bemmerl <mail@mx-server.de>
2014-01-09 03:21:07 +0100

    ISO 3166-1 codes are uppercase.

--- ISO31661Resolver.py
+++ ISO31661Resolver.py
@@ -4,7 +4,7 @@ from WikidataTitleResolver import WikidataTitleResolver
 
 class ISO31661Resolver(WikidataTitleResolver):
     def get_flag(self, code):
-        code = 'ISO 3166-2:{0}'.format(code)
+        code = 'ISO 3166-2:{0}'.format(code.upper())
         
         # Call base class get_flag method
         return super(ISO31661Resolver, self).get_flag(code)
