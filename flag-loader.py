f2195428e04349b2740eaedaf8715c9f70b5fdfe
Michael Bemmerl <mail@mx-server.de>
2014-01-10 01:10:26 +0100

    Implemented downloading of the bitmap version.

--- flag-loader.py
+++ flag-loader.py
@@ -23,7 +23,7 @@ parser = argparse.ArgumentParser(description="A tool for downloading country fla
 parser.add_argument('LIST', nargs='+', help="List of data separated by a space (e.g. 'DE AU')")
 group = parser.add_mutually_exclusive_group()
 group.add_argument('--tld', action='store_true', help="Interpret LIST as Top Level Domain (e.g. '.de')")
-group.add_argument('--name', action='store_true', help="Interpret LIST as official english country name (e.g. 'Germany')")
+group.add_argument('--name', action='store_true', help="Interpret LIST as official English country name (e.g. 'Germany')")
 group.add_argument('--iso-3166-1', action='store_true', help="Interpret LIST as ISO 3166-1 alpha-2 code (e.g. 'DE')")
 
 # More to come: http://www.statoids.com/wab.html
@@ -47,14 +47,31 @@ for entry in args.LIST:
     flag = resolver.get_flag(entry)
 
     if flag is None:
-        parser.exit(status=1, message="No flag found for '{0}' found..".format(entry))
+        parser.exit(status=1, message="No flag found for '{0}' found.".format(entry))
 
     wmc = WikimediaCommons()
-    flag_url = wmc.get_image_url(flag)
+    
+    if args.bitmap:
+        width = args.bitmap_width
+        # Use 200 pixels as default if the width was omitted
+        if width is None:
+            width = 200
+        flag_url = wmc.get_thumb_image_url(flag, width=width)
+    else:
+        flag_url = wmc.get_image_url(flag)
 
     if args.url:
         print flag_url
     else:
-        print "Downloading '{0}'...".format(flag)
-        data = urllib.urlretrieve(flag_url, flag)
+        # Use flag name as file name
+        file = flag
+        bmptxt = ""
+        
+        if args.bitmap:
+            bmptxt = "raster image version of "
+            # Extract file extensions and fiddle all together
+            file = '{0}-{1}px.{2}'.format(flag[:flag.rindex('.')], width, flag_url[flag_url.rindex('.') + 1:])
+            
+        print "Downloading {1}'{0}'...".format(flag, bmptxt)
+        data = urllib.urlretrieve(flag_url, file)
         print "Done."
